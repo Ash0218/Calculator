9121c243d5ee2062cb38fe2932980310d8e19572
Ash0218 <ajykim218@gmail.com>
2019-12-04 17:30:53 -0500

    add local comments

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,9 +12,10 @@ public class Calculator { // one
 
         }
 
-
+        // LOCAL REPO: Comment 1
         public static double divide ( double n1, double n2){ // 5
             double result = n1 / n2; // 6
             return result;
         }
+        // LOCAL REPO: Comment 2
     }
