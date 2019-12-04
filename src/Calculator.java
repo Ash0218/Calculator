ef8371034b65e0825577ca88ee0572af595eef1a
Ash0218 <ajykim218@gmail.com>
2019-12-04 17:07:20 -0500

    merged

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,21 +2,19 @@ public class Calculator { // one
     // this is a very useful method.
     public static double add(double num1, double num2) { // 1
 
-    public static double add(double num1, double num2){ // 1
+        public static double add ( double num1, double num2){ // 1
 
-        return num1 + num2; // 2
-    }
-
-    public static double subtract(double num1, double num2) { // 3
-        return num1 - num2; // 4
-
-    }
-
-    public static double multiply(double num1, double num2) { // 5
-            return num1 * num2; // 6
+            return num1 + num2; // 2
         }
-}
-    public static double divide (double n1, double n2) { // 5
-        double result = n1 / n2; // 6
-        return result;
+
+        public static double subtract ( double num1, double num2){ // 3
+            return num1 - num2; // 4
+
+        }
+
+
+        public static double divide ( double n1, double n2){ // 5
+            double result = n1 / n2; // 6
+            return result;
+        }
     }
